eae72475239c9eb5f1ff94a68bcb6813fa7153a5
亓雪峰 <634123426@qq.com>
2020-04-20 21:25:06 +0800

    docker-compose

--- nginx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-docker stop nginx
-docker rm nginx
-docker run --name nginx -d --restart always --network mynetwork --ip 172.18.0.2 -p 80:80 -v /workspace:/workspace:cached -v /workspace/docker/nginx:/etc/nginx nginx
-
-
--- php71/docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker stop php71
-docker rm php71
-docker run --name php71 -d  --restart always --add-host local.api.zhendecar.com:172.18.0.2 --network mynetwork --ip 172.18.0.3 -v ~/workspace:/workspace:cached   php71:v1
-docker exec php71 sh -c "nohup php /workspace/web/project/local.api.zhendecar.com/artisan queue:work database > /root/local.api.zhendecar.queue.log 2>&1 &"
